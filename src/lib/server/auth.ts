c40c9a22d6784e1e6bcde68f81381b962cf79070
Tanmay Sarkar <sarkartanmay393@gmail.com>
2024-10-28 03:05:54 +0530

    feat: remove proxyRedirect from auth.ts

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -6,7 +6,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub({
     clientId: process.env.AUTH_GITHUB_ID,
     clientSecret: process.env.AUTH_GITHUB_SECRET,
-    redirectProxyUrl: `http://localhost:3000/api/auth/callback/github`,
   })],
   pages: {
     signIn: "/auth",
